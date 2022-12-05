a1b2055cfa3605f97107e37a08c07167ed3bafd1
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2022-12-05 18:09:15 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./components/Sidebar"; //1
+import "./components/Reviews"; //2
+import "./components/Average"; //3
+import "./components/Sentiment"; //4
+import "./components/Website"; //5
+import Sidebar from "./components/Sidebar";
+import Reviews from "./components/Reviews";
+import Average from "./components/Average";
+import Sentiment from "./components/Sentiment";
+import Website from "./components/Website";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Sidebar />
+      <div className="right">
+      <div className="topBar">
+      <Reviews />
+      <Average />
+      <Sentiment />
+      </div>
+      <div className="bottomBar">
+      <Website />
+      </div>
+      </div>
     </div>
   );
 }
